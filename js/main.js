ce9d23ddb95a6ce8d38221cecddc96a26327c5d3
DAN YU <danyu1996@gmail.com>
2015-11-01 15:03:46 -0800

    adding more cool javascript libraries for workshop

--- js/main.js
+++ js/main.js
@@ -11,6 +11,16 @@ $(document).ready(function() {
   // Init the grid for side-projects.
   Grid.init();
 
+  // Init WOW for awesome scroll animations.
+  new WOW().init();
+
+  // Init text rotator for cool text display.
+  $(".rotate").textrotator({
+    animation: "dissolve",
+    separator: ",",
+    speed: 2000
+  });
+
   /* This click listener allows the user to scroll to different parts of the page
    * using the navbar by attaching ids to anchors throughout the page.
    */
@@ -18,7 +28,7 @@ $(document).ready(function() {
     e.preventDefault();
     var hash = this.hash;
     $('html, body').animate({
-      scrollTop: $(this.hash).offset().top - 60
+      scrollTop: $(this.hash).offset().top - 40
     }, 1000, function(){
       window.location.hash = hash;
     });
